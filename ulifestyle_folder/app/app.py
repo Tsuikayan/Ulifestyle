72c20fdf6f1cbaac962fc6369284d61fba424b8c
fungzhi <fungzhi123#gmail.com>
2020-04-13 02:49:45 +0800

    second commit

--- ulifestyle_folder/app/app.py
+++ ulifestyle_folder/app/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, send_from_directory, redirect, url_for, request, Response
+from flask_bootstrap import Bootstrap
+import os
 
 app = Flask(__name__)
+bootstrap = Bootstrap()
+UPLOAD_FOLDER = 'img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('auth/register.html')
 
 
 if __name__ == '__main__':
