6a6b5836c1559511a47ec514d1621527f7da130e
fungzhi <fungzhi123#gmail.com>
2020-04-16 23:05:46 +0800

    add login + link problem solved

--- ulifestyle_folder/app/app.py
+++ ulifestyle_folder/app/app.py
@@ -1,19 +1,36 @@
-from datetime import datetime
-from flask import Flask, render_template, send_from_directory, redirect, url_for, request, Response
+from flask import Flask, render_template, redirect, url_for
+from flask_login import current_user, logout_user
 from flask_bootstrap import Bootstrap
-import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap()
 UPLOAD_FOLDER = 'img'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="首頁")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html', title="會員登記")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    return render_template('login.html', title="會員登入")
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
