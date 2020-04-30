e8c08edc9aec9c4c635eca097e1f08b748a1d04a
fungzhi <fungzhi123#gmail.com>
2020-04-30 22:01:34 +0800

    login,register,db:usertable(u can login with test test123)

--- ulifestyle_folder/app/models.py
+++ ulifestyle_folder/app/models.py
@@ -5,16 +5,34 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 import jwt
-from app import db
+from app import db, login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(30), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String(128))
+    gender = db.Column(db.String(10), nullable=False)
+    phoneNumber = db.Column(db.Integer, nullable=True)
+    year = db.Column(db.Integer, nullable=False)
+    month = db.Column(db.Integer, nullable=False)
+    day = db.Column(db.Integer, nullable=False)
+    educationLevel = db.Column(db.String(30), nullable=True)
+    income = db.Column(db.String(30), nullable=True)
+    usergroup = db.Column(db.String(30), nullable=False)
+    password_hash = db.Column(db.String(30), nullable=False)
+    getinfo = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- ulifestyle_folder/app/routes.py
+++ ulifestyle_folder/app/routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import flash, render_template, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
 from flask_bootstrap import Bootstrap
 from app.forms import *
 from app import app, db
@@ -17,21 +18,45 @@ def index():
     return render_template('index.html', title="首頁")
 
 
-@app.route('/register_test', methods=['GET', 'POST'])
-def register_test():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            gender=form.gender.data,
+            phoneNumber=form.phoneNumber.data,
+            year=form.year.data,
+            month=form.month.data,
+            day=form.day.data,
+            educationLevel=form.educationLevel.data,
+            income=form.income.data,
+            usergroup=form.usergroup.data,
+            getinfo=form.getinfo.data)
+        user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login_test'))
-    return render_template('register_test.html', title="會員登記", form=form)
+        flash('恭喜您成為會員!!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="會員登記", form=form)
 
 
-@app.route('/login_test', methods=['GET', 'POST'])
-def login_test():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-    return render_template('login_test.html', title="會員登入", form=form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+            return redirect(next_page)
+    return render_template('login.html', title="會員登入", form=form)
 
 
 @app.route('/logout')
@@ -43,6 +68,41 @@ def logout():
 @app.route('/blog', methods=['GET', 'POST'])
 def blog():
     return render_template('blog.html', title="博客主頁–U Blog")
+
+
+@app.route('/hk', methods=['GET', 'POST'])
+def hk():
+    return render_template('hk.html', title="HK港生活")
+
+
+@app.route('/travel', methods=['GET', 'POST'])
+def travel():
+    return render_template('travel.html', title="Travel旅遊")
+
+
+@app.route('/food', methods=['GET', 'POST'])
+def food():
+    return render_template('food.html', title="Food美食")
+
+
+@app.route('/beauty', methods=['GET', 'POST'])
+def beauty():
+    return render_template('beauty.html', title="Beauty美容")
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    return render_template('download.html', title="download")
+
+
+@app.route('/video', methods=['GET', 'POST'])
+def video():
+    return render_template('video.html', title="影片頻道")
+
+
+@app.route('/sky_post', methods=['GET', 'POST'])
+def sky_post():
+    return render_template('sky_post.html', title="晴報SkyPost")
 
 
 if __name__ == '__main__':
